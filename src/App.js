a0f64f94601c004f7aebdc054ce1a829f9e64486
변성욱/사원/(주)신세계 <FAD02516@SSG.GLOBAL>
2023-01-10 17:08:12 +0900

    first commit / apply route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let imgWidth;
+    imgWidth = {
+        width: "100%"
+    };
+
+    return (
+        <div className="App">
+
+            {/* 네비게이션 바 */}
+            <Navbar bg="dark" variant="dark">
+                <Container>
+
+                    {/* 로고 이미지 삽입 */}
+                    <Navbar.Brand href="/">LOGO</Navbar.Brand>
+
+                    <Nav className="me-auto">
+                        <Nav className="me-auto">
+                            
+                            {/* 회사 소개 */}
+                            <Nav.Link href="about">About</Nav.Link>
+                            
+                            {/* 제품 이미지 */}
+                            <Nav.Link href="wayToCome">Way to come</Nav.Link>
+                            <NavDropdown title="product" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="/product/1">product-1</NavDropdown.Item>
+                                <NavDropdown.Item href="/product/2">product-2</NavDropdown.Item>
+                                <NavDropdown.Item href="/product/3">product-3</NavDropdown.Item>
+                                {/*<NavDropdown.Divider />*/}
+                            </NavDropdown>
+                        </Nav>
+                    </Nav>
+
+                </Container>
+            </Navbar>
+
+
+            {/* Route 보이는 부분 */}
+            <Container>
+                <Routes>
+                    <Route path="/" element={<h1>main</h1>}></Route>
+                    <Route path="/about" element={<h1>about</h1>}></Route>
+                    <Route path="/wayToCome" element={<h1>way to come</h1>}></Route>
+                    <Route path="/product/1" element={<img style={imgWidth} src={'/images/product-1.jpg'} alt={'product1'}/>}></Route>
+                    <Route path="/product/2" element={<img style={imgWidth} src={'/images/product-2.jpg'} alt={'product2'}/>}></Route>
+                    <Route path="/product/3" element={<img style={imgWidth} src={'/images/product-3.jpg'} alt={'product3'}/>}></Route>
+                </Routes>
+            </Container>
+
+        </div>
+    );
 }
 
 export default App;
